81a7a0c3778a457bc708156d004da4167cea3ab5
Eugene Choi <eugene.maestro.choi@gmail.com>
2014-11-20 20:47:13 +0800

    using environment variable for mm api

--- server/mailer/mailService.js
+++ server/mailer/mailService.js
@@ -12,12 +12,12 @@ module.exports = function (app) {
   console.log('running');
   app.post('/mail', function (req, res) {
     var mail = req.body.address;
-    
+
     request.post({
       url: 'http://api.madmimi.com/audience_lists/test/add?email=' + mail,
       body: {
         username: 'info@locategroup.com',
-        api_key: config.mm_key,
+        api_key: process.env.mm_key || config.mm_key,
       },
       json: true
     }, function (err, httpResponse, body) {
