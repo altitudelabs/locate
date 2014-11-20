1d2b1e399f8b1ad0a998f2cec6c49a426cc817c0
Eugene Choi <eugene.maestro.choi@gmail.com>
2014-11-20 21:07:19 +0800

    syncing main.js up to mm api function addition

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,29 @@
 var counter = 1;
-var switchBackground = function () {
-  counter = counter === 1 ? 2 : 1;
+$('.switch').click(function () {
+  if (counter === 3) {
+    counter = 1;
+  } else {
+    counter++;
+  }
+  console.log(counter);
   var imageElement = $('#index .background-image');
   var url = 'url(/images/background-'+counter+'.jpg)';
   imageElement.css('background-image', url);
-};
+});
+
+$('.email-form form').submit(function(e) {
+  e.preventDefault();
+  var address = $('.email-form form input').val();
+  console.log('email submitted: ', address);
+  
+  $.ajax({
+    type: 'POST',
+    url: '/mail',
+    data: {
+      address: address
+    },
+    success: function(data){
+      console.log('success, ', data);
+    }
+  });
+});
